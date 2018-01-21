fcdeae72b397889787104d678262e21e46442217
FFFSUNDAYNIGHT <32103564+FFFSUNDAYNIGHT@users.noreply.github.com>
2018-01-21 22:12:09 +0800

    修改了部分代码

--- NLP/tool.py
+++ NLP/tool.py
@@ -10,18 +10,15 @@ class Tool:
              json.loads(lzma.open('iposf.json.xz').read().decode('utf-8')))
         self.S = Scissor(json.loads(lzma.open('wf.json.xz').read().decode('UTF-8')), \
                 json.loads(lzma.open('iw.json.xz').read().decode('UTF-8')))
+        
     def cut_mark(self, s):
         result = []
         cut = self.S.Cut(s)
         mark = self.M.Sentemark(cut)
-
-        #print(mark)
-
         for i in range(0,len(cut)):
             result.append(cut[i]+'/'+mark[i])
-        return cut
-        #print(result)
-            #print(cut[i] + '/' + mark[i] +'  ')
+        return result
+    
 if __name__ =='__main__':
     t = Tool()
     print('Enter: ')
