0ba31262d72538d9f525b0cf3368e76f319f3c64
lihuichao <lihuichaolihc@gmail.com>
2018-01-21 17:19:18 +0800

    增加了部分注释

--- NLP/mark.py
+++ NLP/mark.py
@@ -1,13 +1,13 @@
 class  Mark:
     '''
-    中文分词
+    词性标注
     '''
     def __init__(self, dic_word = {}, dic_first = {}, dic_hmm = {}):
         '''
-        构造一个中文分词类
-        :param dic_word:
-        :param dic_first:
-        :param dic_hmm:
+        构造一个词性标注类，对list进行词性标注
+        :param dic_word:查询一个字所有词性的词典
+        :param dic_first:查询第一个词相关信息的词典
+        :param dic_hmm:包含条件概率信息的词典
         '''
         self.dic_word = dic_word
         self.dic_first = dic_first
@@ -15,9 +15,9 @@ class  Mark:
 
     def Wordtype(self, word):
         '''
-        Get type of certain word
-        :param word:
-        :return:part of speech as a list
+        获得一个词所有可能的词性
+        :param word:待查询的词
+        :return:该词所有可能词性的list
         '''
         try:
             wordtypes = self.dic_word[word]
@@ -27,10 +27,10 @@ class  Mark:
         
     def Firstword(self, word, Wordtag):
         '''
-
-        :param word:
-        :param Wordtag:
-        :return:
+        获取list中第一个词的词性
+        :param word:第一个词
+        :param Wordtag:和输入list对应的词性list
+        :return:第一个词的所有可能词性
         '''
         try:
             max = 0
@@ -40,7 +40,7 @@ class  Mark:
                     max = p
                     ptag = tag
             Wordtag.append(ptag)
-            '如果该词在词典中第一个出现的概率为零'
+            #如果该词在第一个位置未出现过
         except KeyError:
             ptag = self.Wordtype(word)
             if type(ptag) is list:
@@ -52,10 +52,10 @@ class  Mark:
                 Wordtag.append(ptag)
     def Wordmark(self, index, folword, Wordtag):
         '''
-        Marking word by HMM
-        :param index: the position of
-        :param folword:
-        :param Wordtag:
+        用HMM模型进行词性标注
+        :param index:上一个词在list中的位置
+        :param folword:待查询的词
+        :param Wordtag:同上
         :return:
         '''
         wordtypes = self.Wordtype(folword)
@@ -73,9 +73,9 @@ class  Mark:
 
     def Sentemark(self, wordseq = []):
         """
-
-        :param wordseq:
-        :return:
+        对一个list进行分词
+        :param wordseq:输入的list
+        :return:Wordtag
         """
         Wordtag = []
         self.Firstword(wordseq[0], Wordtag)
